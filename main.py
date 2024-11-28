20084753388f02c02c1fb98125ecb4de3818fcc5
Ast3risk-ops <spacedude609@pm.me>
2024-11-27 23:14:11 -0500

    Remove success message and add send date field
    Signed-off-by: Ast3risk-ops <spacedude609@pm.me>

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class TagSet(discord.ui.Modal):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         # Modal for tag input
-        self.add_item(discord.ui.InputText(label="Tag(s)", required=False))
+        self.add_item(discord.ui.InputText(label="", required=False))
 class DeleteBookmark(discord.ui.View):
     @discord.ui.button(label="", style=discord.ButtonStyle.secondary, emoji="🗑️")
     async def button_callback(self, button, interaction):
@@ -23,21 +23,21 @@ class DeleteBookmark(discord.ui.View):
         discord.IntegrationType.guild_install,
         discord.IntegrationType.user_install,
     },
-   name="Add to Bookmarks"
+   name="Archive Message"
 )
 async def bookmark_tag(
     ctx,
     message: discord.Message
 ):
-    modal = TagSet(title="Tag(s) to save with")
+    modal = TagSet(title="Message Tag(s)")
     await ctx.send_modal(modal)
     await modal.wait() # Wait for the modal to finish
-    await ctx.respond(f"🔖 Tagged!", ephemeral=True)
-    embed = discord.Embed(title="<:mdibookmark:1311457777834528768> Bookmark", description=f"{message.content}", timestamp=message.created_at, color=discord.Colour.random())
+    embed = discord.Embed(title="<:mdiarchive:1311542586745294868> Archived Message", description=f"{message.content}", timestamp=message.created_at, color=discord.Colour.random())
     embed.add_field(name="<:mdiaccount:1311490376091045989> Author", value=f"<@{message.author.id}>", inline=True)
     embed.add_field(name="<:mdilinkvariant:1311490590747267082> Link", value=f"{message.jump_url}", inline=True)
     embed.add_field(name="<:mdicodetags:1311506780332752977> ID", value=f"`{message.id}`", inline=True)
     embed.add_field(name="<:mditag:1311505882047189012> Tags", value=f"{modal.children[0].value}", inline=True)
+    embed.add_field(name="<:mdicalendar:1311544097240121384> Send Date", value="", inline=False)
     embed.set_thumbnail(url=f"{message.author.avatar.url}")
     await ctx.user.send(embed=embed, view=DeleteBookmark())
     for i in message.attachments:
