92cbbfae51e03e4ea049ac69595d62d2a23b3569
Ast3risk-ops <spacedude609@pm.me>
2024-11-30 09:11:57 -0500

    better embed spacing

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class ColourModal(discord.ui.Modal):
 
         # Validate the color input
         if not color_input.startswith("#") or len(color_input) != 7:
-            await interaction.response.send_message("Please enter a valid hex color code (e.g., #FF5733).", ephemeral=True)
+            await interaction.response.send_message("Please enter a valid hex code (e.g., `#FF5733`).", ephemeral=True)
             return
 
         # Update the global embed color
@@ -45,9 +45,11 @@ class DeleteBookmark(discord.ui.View):
     async def button_callback(self, button, interaction):
         self.disable_all_items()
         await interaction.message.delete()
+        return
     @discord.ui.button(label="", style=discord.ButtonStyle.secondary, emoji="📌")
     async def pin_callback(self, button, interaction):
         await interaction.message.pin()
+        return
     @discord.ui.button(label="", style=discord.ButtonStyle.secondary, emoji="⬇️")
     async def button2_callback(self, button, interaction):
         for i in interaction.message.embeds:
@@ -55,9 +57,11 @@ class DeleteBookmark(discord.ui.View):
             await interaction.message.reply(embed=i, view=DeleteBookmark())
         for i in interaction.message.attachments:
             await interaction.message.reply(i)
+        return
     @discord.ui.button(label="", style=discord.ButtonStyle.secondary, emoji="🎨")
     async def customizer(self, button, interaction):
         await interaction.response.send_modal(ColourModal())
+        return
 class About(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
@@ -114,16 +118,17 @@ async def bookmark_tag(
         formatted_emoji = str(i.emoji)
         reactionlist.append(f"{count}x {formatted_emoji} ")
     global embed
-    embed = discord.Embed(title=f"<:mdiarchive:1311542586745294868> Archived Message On {discord.utils.format_dt(dt.now(), 'f')} ({discord.utils.format_dt(dt.now(), 'R')})", description=f"{message.content}", color=discord.Colour.random())
-    embed.add_field(name="\n", value=", ".join(reactionlist), inline=False)
+    embed = discord.Embed(title=f"<:mdiarchive:1311542586745294868> Archived Message On {discord.utils.format_dt(dt.now(), 'f')} ({discord.utils.format_dt(dt.now(), 'R')})", description=f"-------\n\n{message.content}\n\n--------", color=discord.Colour.random())
+    embed.add_field(name="\n\n", value=", ".join(reactionlist), inline=False)
     embed.add_field(name="<:mdiaccount:1311490376091045989> Author", value=f"<@{message.author.id}>", inline=True)
     embed.add_field(name="<:mdilinkvariant:1311490590747267082> Link", value=f"{message.jump_url}", inline=True)
     embed.add_field(name="<:mdicodetags:1311506780332752977> ID", value=f"`{message.id}`", inline=True)
     if message.embeds:
         numembeds = len(message.embeds) # Number of embeds
         embed.add_field(name="<:mdicardtext:1311825458480021596> Embeds", value=f"{numembeds}", inline=True)
+    embed.add_field(name="<:mdicalendar:1311544097240121384> Send Date", value=f"{discord.utils.format_dt(message.created_at, 'F')}", inline=True)
     embed.add_field(name="<:mditag:1311505882047189012> Tags", value=f"{modal.children[0].value}", inline=True)
-    embed.add_field(name="<:mdicalendar:1311544097240121384> Send Date", value=f"{discord.utils.format_dt(message.created_at, 'F')}", inline=False)
+
     embed.set_thumbnail(url=f"{message.author.avatar.url}")
     await ctx.user.send(embed=embed, view=DeleteBookmark())
     for i in message.attachments:
