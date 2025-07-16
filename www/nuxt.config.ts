53b157287c7a07e646916cce8bafd7f2e98bd70b
Ast3risk-ops <spacedude609@pm.me>
2025-07-15 20:54:37 -0400

    maybe?

--- www/nuxt.config.ts
+++ www/nuxt.config.ts
@@ -37,7 +37,7 @@ export default defineNuxtConfig({
     },
     preset: "cloudflare_pages",
     cloudflare: {
-      deployConfig: true,
+      deployConfig: false,
       nodeCompat: true,
       wrangler: { "name": "archiver-www" }
     }
